5e3b7d53cf4931d9d25e586d2c2b33c311633135
Mudit_Singh <muditsingh1904@gmail.com>
2025-08-03 10:40:43 +0530

    Add files via upload

---
+++ Book_Allocation.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPossible(vector<int>a,int mid,int n,int m){
+    int Student_count=1;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        if(sum+a[i]<=mid){
+            sum+=a[i];
+        }
+        else if(Student_count++>m || a[i] > mid){
+            return false;
+        }
+        else{
+            Student_count++;
+            sum=a[i];
+        }
+    }
+    return true;
+
+}
+
+int book_allocation(vector<int>a,int n,int m){
+    int s=0;
+    int e=0;
+    int ans=0;
+    for(int i=0;i<n;i++){
+        e+=a[i];
+    }
+    int mid =s+(e-s)/2;
+
+    while(s<=e){
+        if (isPossible(a,mid,n,m)){
+            ans=mid;
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
+int main(){
+    vector<int>a={10,20,30,40};
+    int n=4;
+    int m=2;
+    cout << "Answer is = " << book_allocation(a,n,m);
+
+}
